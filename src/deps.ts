6f06ef8f72a63c878adfd54fa184e1049509c691
kaiiy <81578246+kaiiy@users.noreply.github.com>
2024-02-06 16:59:35 +0900

    Update dependencies for openai, tiktoken, and colors

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
 export * as cliffy from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
-export * as openai from "https://deno.land/x/openai@v4.26.0/mod.ts";
-export * as tiktoken from "npm:@dqbd/tiktoken@1.0.7";
-export * as colors from "https://deno.land/std@0.213.0/fmt/colors.ts";
+export * as openai from "https://deno.land/x/openai@v4.26.1/mod.ts";
+export * as tiktoken from "npm:@dqbd/tiktoken@1.0.13";
+export * as colors from "https://deno.land/std@0.214.0/fmt/colors.ts";
 export * as prompts from "npm:@clack/prompts@0.7.0";
